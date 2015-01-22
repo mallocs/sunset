9a0354f2ab312a3c9bca98f4621eb4f4d11a840a
Marcus Ulrich <ulrich@mallocs.net>
2015-01-21 20:41:04 -0800

    Updating slideshow helper

--- helpers.js
+++ helpers.js
@@ -2,10 +2,24 @@ var hbs = require('express-hbs');
 var fs = require('fs');
 var slideshows = require('./slideshows.json');
 
+/***
+Articles should have a directory defined in slideshows.json or we check for a
+directory with the same name as the articleId.
+***/
 registerHelper = function (){
     hbs.registerHelper("slideshow", function(articleId) {
-        var dir = slideshows["rootDir"] + slideshows["articles"][articleId]["imgDir"];
-        var urlPath = slideshows["urlPath"] + slideshows["articles"][articleId]["imgDir"];
+        if (slideshows.articles && slideshows.articles[articleId] &&
+            slideshows.articles[articleId].imgDir) {
+            var dir = slideshows.rootDir + slideshows.articles[articleId].imgDir;
+            var urlPath = slideshows.urlPath + slideshows.articles[articleId].imgDir;
+        } else {
+            var dir = slideshows.rootDir + articleId + "/";
+            var urlPath = slideshows.urlPath + articleId + "/";
+        }
+
+        if (!fs.existsSync(dir)) {
+            return new hbs.SafeString(' ');
+        }
         var dirList = fs.readdirSync(dir);
         var out = '<div class="mmi-slideshow">'
                 + '<ul class="carousel">\n';
