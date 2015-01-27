98fbf21cac4c11e730670c40a96e31e1020408db
Marcus Ulrich <ulrich@mallocs.net>
2015-01-26 22:38:01 -0800

    Making sure to only include jpg, gif, and png files.

--- helpers.js
+++ helpers.js
@@ -1,5 +1,6 @@
 var hbs = require('express-hbs');
 var fs = require('fs');
+var path = require('path');
 var slideshows = require('./slideshows.json');
 
 /***
@@ -25,28 +26,29 @@ registerHelper = function (){
 
         var dirList = fs.readdirSync(dir);
         if (fs.existsSync(dir + "sprite.jpg")) {
-            var out = '<div class="mmi-slideshow"';
+            var out = '<div class="mmi-slideshow" ';
                 out += 'data-pagination="sprite" data-sprite="' + urlPath + 'sprite.jpg">';
         } else {
             var out = '<div class="mmi-slideshow">';
         }
 
         for (var i=0, l=dirList.length; i<l; i++) {
-            var filePath = dir + dirList[i];
-            if (fs.lstatSync(filePath).isDirectory() ||
-                dirList[i] === "sprite.jpg") {
-                continue;
-            }
-            if (i == 0) {
-                out += '<ul class="carousel">\n';
-                out += '<li class="slide">\n';
-                out += '<img src="' + urlPath + dirList[0] + '">\n';
-                out += '</li>\n';
-
-            } else {
-                out += '<li class="slide">\n';
-                out += '<img data-src="' + urlPath + dirList[i] + '">\n';
-                out += '</li>\n';
+            var fileName = dirList[i];
+            var filePath = dir + fileName;
+            var ext = path.extname(fileName).toLowerCase();
+            //only print for jpg, gif, or png files
+            if (!fs.lstatSync(filePath).isDirectory() &&
+               (ext === ".jpg" || ext === ".gif" || ext === ".png")) {
+                if (i == 0) {
+                    out += '<ul class="carousel">\n';
+                    out += '<li class="slide">\n';
+                    out += '<img src="' + urlPath + fileName + '">\n';
+                    out += '</li>\n';
+                } else {
+                    out += '<li class="slide">\n';
+                    out += '<img data-src="' + urlPath + fileName + '">\n';
+                    out += '</li>\n';
+                }
             }
         }
         return new hbs.SafeString(out + '</ul>\n</div>\n');
